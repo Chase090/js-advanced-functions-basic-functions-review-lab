a17ece4c201d73689ec9701c0048f7e82afa51f8
Chase090 <carloyam21@icloud.com>
2021-07-18 02:52:12 -0700

    commit tree ee2399a27d47de30974717a60f81fe50e19de4c2

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+};
+
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`
+};
+
+function wrapAdjective(p = '*') {
+    return function(adjective) {
+        return `You are ${p}${adjective}${p}!`
+
+    };
+};
+
+const Calculator = {
+ multiply: function(num1, num2) { return num1 * num2},
+ divide: function(num1, num2) { return num1 / num2},
+ add: function(num1, num2) { return num1 + num2},
+ subtract: function(num1, num2) { return num1 - num2},
+
+};
+
+let actionApplyer = function (start, ray) {
+    let a = start;
+  
+    for (let i = 0; i < ray.length; i++) {
+      a = ray[i](a);
+    }
+  
+    return a;
+  };
